9d9e84b026f36be723db6563c607168eebe0e1fb
ayalla323 <ayalla323@gmail.com>
2021-12-11 20:04:05 +0200

    PlayerMove

---
+++ Assets/script/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speedMove = 300;
+
+    private Rigidbody rigidbody;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        Vector3 move = new Vector3(horizontal, 0, vertical);
+        
+        rigidbody.AddForce(move * speedMove * Time.deltaTime);
+    }
+}
